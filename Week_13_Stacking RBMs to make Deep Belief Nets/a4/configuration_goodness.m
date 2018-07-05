a9a827756accc9cad5cfaa023e6238f49c037b07
Nguyen Bao Hung <33221318+nguyenbh1507@users.noreply.github.com>
2018-07-05 11:32:09 +0700

    weeks 12-16

---
+++ Week_13_Stacking RBMs to make Deep Belief Nets/a4/configuration_goodness.m
@@ -0,0 +1,10 @@
+function G = configuration_goodness(rbm_w, visible_state, hidden_state)
+% <rbm_w> is a matrix of size <number of hidden units> by <number of visible units>
+% <visible_state> is a binary matrix of size <number of visible units> by <number of configurations that we're handling in parallel>.
+% <hidden_state> is a binary matrix of size <number of hidden units> by <number of configurations that we're handling in parallel>.
+% This returns a scalar: the mean over cases of the goodness (negative energy) of the described configurations.
+ G = hidden_state * visible_state';
+ G = G .* rbm_w;
+ G = sum(sum(G))/size(visible_state, 2);
+ %error('not yet implemented');
+end
